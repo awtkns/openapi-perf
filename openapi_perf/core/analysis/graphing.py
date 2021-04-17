17884e0a814b95cb109adf54662cead82f1b1297
Adam Watkins <cadamrun@gmail.com>
2021-04-16 17:11:12 -0700

    :fire: Update Styling

--- openapi_perf/core/analysis/graphing.py
+++ openapi_perf/core/analysis/graphing.py
@@ -11,8 +11,18 @@ import pandas as pd
 
 from scipy.stats import gaussian_kde
 
-custom_cycler = (cycler(color=["#30a2da", "#fc4f30"]))
+custom_cycler = cycler(color=["#30a2da", "#fc4f30"])
 plt.style.use("seaborn-whitegrid")
+
+# TODO Define this properly
+BOXPLOT_STYLE = dict(
+    boxprops=dict(linewidth=1.5, color="#30a2da"),
+    flierprops=dict(marker=".", markeredgecolor="#fc4f30", markersize=3),
+    medianprops=dict(linewidth=1.5, color="#fc4f30"),
+    whiskerprops=dict(linewidth=1.5, color="#30a2da"),
+    capprops=dict(linewidth=1.5, color="#fc4f30"),
+)
+
 
 def get_dims(n_plots: int) -> Tuple[int, int]:
     """
@@ -22,14 +32,14 @@ def get_dims(n_plots: int) -> Tuple[int, int]:
     """
 
     if n_plots % 2:
-        n_plots =+ 1
+        n_plots = +1
 
     return int(n_plots / 2), 2
 
 
 def generate_graphs(results: pd.DataFrame) -> None:
     df = results
-    df = df[df['time'] < 0.05]
+    df = df[df["time"] < 0.05]
 
     routes: pd.DataFrame = df["path_name"].unique()
 
@@ -41,8 +51,13 @@ def generate_graphs(results: pd.DataFrame) -> None:
         ax.set_prop_cycle(custom_cycler)
 
         df[(df.path_name == route)].boxplot(
-            by=["type", "status_code"], column="time", rot=30, ax=ax
+            by=["type", "status_code"],
+            column="time",
+            rot=30,
+            ax=ax,
+            **BOXPLOT_STYLE
         )
+
         ax.title.set_text(f"Route: {route}")
         ax.ylabel = "Response Time (s)"
 
@@ -51,7 +66,9 @@ def generate_graphs(results: pd.DataFrame) -> None:
     plt.show()
 
 
-def _add_kde(ax, data, xlim: Tuple[float, float] = None, samples: int = 1000, label: str = None):
+def _add_kde(
+        ax, data, xlim: Tuple[float, float] = None, samples: int = 1000, label: str = None
+):
     kde = gaussian_kde(data)
     xx = np.linspace(*xlim, samples)
     ax.plot(xx, kde.pdf(xx), label=label, linewidth=3)
@@ -61,24 +78,31 @@ def _get_xlim(data1: pd.Series, data2: pd.Series) -> Tuple[float, float]:
     x1 = min(data1.min(), data2.min())
     x2 = max(data1.max(), data2.max())
 
-    return x1, x2
+    return x1 * 1000, x2 * 1000
 
 
-# def _drop_percentiles(data: pd.DataFrame, column: str = "time", percentile: float = 0.99) -> pd.DataFrame:
-#     desc = data[[column]].describe(percentiles=[0.99])
-#     print(desc.iloc)
+def _drop_percentiles(
+        data: pd.DataFrame, column: str = "time", percentile: float = 0.995
+) -> pd.DataFrame:
+    high = data[column].quantile(percentile)
+    low = data[column].quantile(1 - percentile)
+
+    return data[(data[column] < high) & (data[column] > low)]
 
 
 def plot_regression(new: pd.DataFrame, old: pd.DataFrame):
-    # _drop_percentiles(new)
-    new = new[new['time'] < 0.05] # TODO: REMOVE
-    old = old[old['time'] < 0.05]
+    new = _drop_percentiles(new)
+    old = _drop_percentiles(old)
 
     new_routes = new.drop_duplicates(["path_name", "type"])[["path_name", "type"]]
     old_routes = old.drop_duplicates(["path_name", "type"])[["path_name", "type"]]
-    shared_routes = pd.merge(new_routes, old_routes, how='inner', on=['path_name', 'type'])
+    shared_routes = pd.merge(
+        new_routes, old_routes, how="inner", on=["path_name", "type"]
+    )
 
-    fig, axes = plt.subplots(*get_dims(len(shared_routes)), figsize=(10, 8), sharex=True)
+    fig, axes = plt.subplots(
+        *get_dims(len(shared_routes)), figsize=(10, 8), sharex=True
+    )
     axes = list(chain(*axes))  # flatten axis array
 
     for i, (_, row) in enumerate(shared_routes.iterrows()):
@@ -88,10 +112,19 @@ def plot_regression(new: pd.DataFrame, old: pd.DataFrame):
         ax_kde.set_prop_cycle(custom_cycler)
         axes.append(ax_kde)
 
-        new_time = new[(new['path_name'] == row.path_name) & (new['type'] == row.type)]['time'] * 1000
-        old_time = old[(old['path_name'] == row.path_name) & (old['type'] == row.type)]['time'] * 1000
-        xlim = _get_xlim(new_time, old_time) # TODO: Potential edge case if the min / max arent in the shared set of routes
+        new_time = (
+                new[(new["path_name"] == row.path_name) & (new["type"] == row.type)]["time"]
+                * 1000
+        )
+        old_time = (
+                old[(old["path_name"] == row.path_name) & (old["type"] == row.type)]["time"]
+                * 1000
+        )
+        xlim = _get_xlim(
+            new["time"], old["time"]
+        )  # TODO: Potential edge case if the min / max arent in the shared set of routes
 
+        # Plotting
         ax.hist(new_time, alpha=0.5, bins=20, label="New")
         ax.hist(old_time, alpha=0.5, bins=20, label="Old")
         _add_kde(ax_kde, new_time, xlim=xlim, label="New PDF")
@@ -104,13 +137,22 @@ def plot_regression(new: pd.DataFrame, old: pd.DataFrame):
 
     fig.suptitle("OpenAPI Regression Analysis", fontsize=20)
     lines_labels = [ax.get_legend_handles_labels() for ax in axes]
-    lines, labels = [sum(lol, []) for lol in zip(*lines_labels[11:13])]  # TODO: THIS IS A HACK, does not generalize
+    lines, labels = [
+        sum(lol, []) for lol in zip(*lines_labels[11:13])
+    ]  # TODO: THIS IS A HACK, does not generalize
     fig.legend(lines, labels, ncol=2)
     fig.autofmt_xdate(rotation=30)
 
-    fig.text(0.5, 0.01, 'Response Time (ms)', ha='center', fontweight='bold')
-    fig.text(0.01, 0.5, 'Count', va='center', rotation='vertical', fontweight='bold')
-    fig.text(0.98, 0.5, 'Probability Density', va='center', rotation='vertical', fontweight='bold')
+    fig.text(0.5, 0.01, "Response Time (ms)", ha="center", fontweight="bold")
+    fig.text(0.01, 0.5, "Count", va="center", rotation="vertical", fontweight="bold")
+    fig.text(
+        0.98,
+        0.5,
+        "Probability Density",
+        va="center",
+        rotation="vertical",
+        fontweight="bold",
+    )
 
-    plt.tight_layout(rect=(.010, .025, .990, .995))
+    plt.tight_layout(rect=(0.010, 0.025, 0.990, 0.995))
     plt.show()
